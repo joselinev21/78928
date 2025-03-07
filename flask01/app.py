beb395e7abe8f978cda8513deb3444f1098d06f5
joselinev21 <joselinevazquezfernandez21@gmail.com>
2025-03-07 10:39:00 -0600

    rutas en flask01

--- flask01/app.py
+++ flask01/app.py
@@ -6,9 +6,30 @@ app = Flask(__name__)
 #app es la instancia q tiene de flask
 #nos permite exponer una funcion para que se ejecute desde web
 
-@app.route('/')
+@app.route('/saludar')
 def hola_mundo():
     return 'Hola mundo!'
+
+@app.route('/despedir')
+def adios_mundo():
+    return 'Adiós mundo!'
+
+@app.route('/hablar')
+def hola_hola():
+    return 'Hola hola!'
+
+@app.route('/hola')
+def hola_html():
+    return '<h1 style="color:red;">Hola!!</h1>'
+
+@app.route('/json')
+def json():
+    return '{"nombre":"john"}'
+
+@app.route('/xml')
+def xml():
+    return '<?xml version="1.0"?><nombre>John</nombre>'
+
 if __name__=='__main__':
     app.run(host='0.0.0.0',
 debug=True)
